5ebfc7367aa3994295b9a59571e3e42d74af333d
Alberto Monroy <monroyalberto@gmail.com>
2023-02-02 21:40:40 -0600

    Add a .gitignore file

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+    region = "us-east-2"
+}
+
+data "aws_ami" "ubuntu" {
+    most_recent = true
+
+    filter {
+        name = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    }
+    owners = ["099720109477"]
+}
+
+resource "aws_instance" "example" {
+    ami = data.aws_ami.ubuntu.id 
+    instance_type = "t2.micro"
+    tags = {
+        Name = "terraform-example"
+    }
+}
