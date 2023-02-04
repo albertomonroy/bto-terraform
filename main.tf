7eeeaa1f664959bb11a61487a782837139b7afed
Alberto Monroy <monroyalberto@gmail.com>
2023-02-03 22:58:00 -0600

    Use of a variable to assign a server port

--- main.tf
+++ main.tf
@@ -1,38 +1,45 @@
 provider "aws" {
-    region = "us-east-2"
+  region = "us-east-2"
 }
 
 data "aws_ami" "ubuntu" {
-    most_recent = true
+  most_recent = true
 
-    filter {
-        name = "name"
-        values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-    }
-    owners = ["099720109477"]
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+  owners = ["099720109477"]
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = "8080"
 }
 
 resource "aws_instance" "example" {
-    ami = data.aws_ami.ubuntu.id 
-    instance_type = "t2.micro"
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
-    user_data = <<-EOF
+  user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello, World" > index.html
                 nohup busybox httpd -f -p 8080 &
                 EOF
 
-    tags = {
-        Name = "terraform-example"
-    }
+  tags = {
+    Name = "terraform-example"
+  }
 }
 
 resource "aws_security_group" "instance" {
-    name = "terraform-example-instance"
-    ingress {
-        from_port = 8080
-        to_port = 8080
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  name = "terraform-example-instance"
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
